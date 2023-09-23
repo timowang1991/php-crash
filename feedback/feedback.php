394a2ff4e19403d8ff29b1ac18cbd3e1c60acad3
Timo Wang <timowang1991@gmail.com>
2023-09-23 21:17:03 +0800

    feedback page done

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,17 +1,22 @@
 <?php include 'inc/header.php'; ?>
 
+<?php
+    $sql = 'SELECT * FROM feedback';
+    $result = mysqli_query($conn, $sql);
+    $feedbacks = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
 <h2>Feedback</h2>
-<div class="card my-3">
-    <div class="card-body">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente praesentium, ipsum, minima reprehenderit quibusdam eligendi quam nostrum ullam nemo consequatur maiores esse velit eaque facere ex vel beatae, aliquid suscipit?
-    </div>
-</div>
 
-<div class="card my-3">
-    <div class="card-body">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente praesentium, ipsum, minima reprehenderit quibusdam eligendi quam nostrum ullam nemo consequatur maiores esse velit eaque facere ex vel beatae, aliquid suscipit?
+<?php foreach($feedbacks as $item): ?>
+    <div class="card my-3 w-75">
+        <div class="card-body text-center">
+            <?php echo $item['body']; ?>
+            <div class="text-secondary mt-2">
+                By <?php echo $item['name']; ?> on <?php echo $item['date']; ?>
+            </div>
+        </div>
     </div>
-</div>
-
+<?php endforeach; ?>
 
 <?php include 'inc/footer.php'; ?>'
