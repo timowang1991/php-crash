9f70a07c9e2ed804d229d39b5e4ae8380b15fed8
Timo Wang <timowang1991@gmail.com>
2023-09-30 22:01:21 +0800

    rounding number

---
+++ php-crash/22_rounding_numbers.php
@@ -0,0 +1,12 @@
+<?php
+
+echo (int) 5.454 . '<br>';
+
+echo floor(5.454) . '<br>';;
+echo floor(-0.1) . '<br>';;
+echo ceil(5.454) . '<br>';
+
+echo round(5.454, 2) . '<br>';
+echo round(5.455, 2) . '<br>';
+echo round(-5.455, 2) . '<br>';
+?>
