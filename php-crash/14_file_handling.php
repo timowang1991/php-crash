0cdbb33eb7a2ec7ab2b6d74e948bfdd4c74d947d
Timo Wang <timowang1991@gmail.com>
2023-10-02 22:13:52 +0800

    working with files

--- php-crash/14_file_handling.php
+++ php-crash/14_file_handling.php
@@ -13,4 +13,22 @@
         fwrite($handle, $contents);
         fclose($handle);
     }
+
+    $directory = scandir(__DIR__);
+    echo '<pre>';
+    print_r($directory);
+    echo '</pre>';
+
+    mkdir('foo');
+    rmdir('foo'); // need foo directory be empty
+
+    if (file_exists($file))  {
+        echo filesize($file) . '<br>';
+
+        file_put_contents($file, 'Hello World');
+
+        clearstatcache(); // refresh file size cache
+
+        echo filesize($file) . '<br>';
+    }
 ?>
